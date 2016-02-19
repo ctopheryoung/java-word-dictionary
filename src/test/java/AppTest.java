49d2b8e7a8564843d5d54539fee13d5aefa2befa
Chris Young <ctopheryoung@gmail.com>
2016-02-19 13:04:49 -0800

    write route for root directory and displaying index.vtl, passes test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,10 +1,11 @@
 import org.fluentlenium.adapter.FluentTest;
-import org.junit.*;
+import org.junit.ClassRule;
+import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.fluentlenium.core.filter.FilterConstructor.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -17,6 +18,10 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-
-  //Tests go here
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Make your own dictionary!");
+  }
+  
 }
