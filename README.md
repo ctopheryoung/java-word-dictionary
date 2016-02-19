981e14bdc373a9e063b584c8a3351c799128d0fe
Chris Young <ctopheryoung@gmail.com>
2016-02-19 09:21:45 -0800

    create initial files with README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Dictionary Web App
+
+#### February 19th, 2016
+
+#### By Chris Young
+
+## Description
+
+This is a dictionary web application. It allows users to create a dictionary by adding words to the dictionary with the ability to add multiple definitions for each word.
+
+## How To Use
+
+* Check out my GitHub repository:
+* See the functioning web app hosted on Heroku:
+
+## Technologies Used
+
+Java, Gradle, Spark, JUnit, FluentLenium, Velocity, Heroku, Bootstrap
---
+++ src/test/java/IntegrationTest.java
@@ -0,0 +1,22 @@
+import org.fluentlenium.adapter.FluentTest;
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+public class IntegrationTest extends FluentTest {
+  public WebDriver webDriver = new HtmlUnitDriver();
+
+  @Override
+  public WebDriver getDefaultDriver() {
+      return webDriver;
+  }
+
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+
+  //Tests go here
+}
