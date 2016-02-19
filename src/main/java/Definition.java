cf253a4483d9105e0c8d617ec25299b15bd0e1cb
Chris Young <ctopheryoung@gmail.com>
2016-02-19 10:40:50 -0800

    create Definition member variables and constructor

--- src/main/java/Definition.java
+++ src/main/java/Definition.java
@@ -1,3 +1,16 @@
 import java.util.ArrayList;
 
-public class Definition { }
+public class Definition {
+
+  private static ArrayList<Definition> instances = new ArrayList<Definition>();
+
+  private String mMeaning;
+  private int mId;
+
+  public Definition(String meaning) {
+    mMeaning = meaning;
+    instances.add(this);
+    mId = instances.size();
+  }
+
+}
